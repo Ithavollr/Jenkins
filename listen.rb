797f0853909c6ad6c78c9caec8830641704c6a3d
Peter Pan <peter@never.lan>
2025-08-16 17:30:44 -0400

    pacman almost done

--- listen.rb
+++ listen.rb
@@ -2,21 +2,31 @@ require 'sinatra'
 require 'json'
 
 SECRET_TOKEN = ENV["SECRET_TOKEN"] || abort("ERROR: SECRET_TOKEN environment variable not set")
-
+## SAMPLE CURL COMMAND:
+# curl -X POST http://localhost:8085/receive \
+#  -H "Content-Type: application/json" \
+#  -d '{"one":1,"two":2,"token":"supersecrettoken123"}'
 set :bind, '127.0.0.1' # Allow external connections
 set :port, 8085
 
-post '/receive' do
-  request.body.rewind
-  data = JSON.parse(request.body.read) rescue {}
-
-  if data["token"] != SECRET_TOKEN
-    halt 403, "Forbidden: Invalid token\n"
+# Run before each request
+before do
+  if request.request_method == "POST"
+    request.body.rewind
+    @data = JSON.parse(request.body.read) rescue {}
+    if @data["token"] != SECRET_TOKEN
+      halt 403, "Forbidden: Invalid token\n"
+    end
   end
+end
 
-  one = data["one"]
-  two = data["two"]
-
+post "/pacman" do
+  # @data is available here
+  run_id = @data["run_id"]
+  data_artifact_id = @data["data_artifact_id"]
+  asset_artifact_id = @data["asset_artifact_id"]
   puts "Received values -> one: #{one}, two: #{two}"
   "Success: one=#{one}, two=#{two}\n"
+  %x(wget -q -P cache/packs/rpack/ '#{url}')
+
 end
